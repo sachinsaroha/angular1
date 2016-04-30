3b17fb53add42daa504c5d625a06515c21f0510e
Sumit Dabas <sumit@sumitMac.local>
2016-04-30 21:22:43 +1000

    commit

---
+++ app/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('myApp', []);
+
+app.controller('myController', [ '$scope', function($scope){
+
+	$scope.toggle= false;
+
+	$scope.customers = [
+						{'name':'Ross', 'age':'29'},
+						{'name':'Chandler', 'age':'25'},	
+						{'name':'Joey', 'age':'29'}
+					   ];
+
+	$scope.show = function(){
+		$scope.toggle = true; 
+	};
+	$scope.hide = function(){
+		$scope.toggle = false; 
+	};
+
+	$scope.name1 = 'Sumit Dabas';
+}]);
